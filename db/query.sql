6555c5ecaeb05a077c975d9d0c977b3ba8fdb0c3
Emily Burger <eburger939@gmail.com>
2022-01-17 13:18:07 -0600

    added select in query.sql for table information, joined department to role and role to employee

---
+++ db/query.sql
@@ -0,0 +1,15 @@
+-- query for view departments
+select *
+from employee_tracker.department;
+
+-- query to view request info from roles 
+-- joined with name of department from department table
+SELECT roleE.id, roleE.title, roleE.salary, department.name
+FROM employee_tracker.roleE
+join department ON roleE.department_id = department.id;
+
+-- query to view employees
+-- does not include department name!!!!
+SELECT employee.id, employee.first_name, employee.last_name, roleE.title, roleE.salary, employee.manager_id
+FROM employee_tracker.employee
+join roleE ON employee.role_id = roleE.id;
