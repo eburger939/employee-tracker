3ab982b7e5ee960b2d3aebefd4296bf54a2a199d
Emily Burger <eburger939@gmail.com>
2022-01-16 20:24:37 -0600

    tables are created within workbench, values added, working code copied into schema.sql and seed.sql

--- db/schema.sql
+++ db/schema.sql
@@ -8,22 +8,22 @@ CREATE TABLE department (
   name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
+CREATE TABLE roleE (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title varchar(30),
   salary decimal,
-  department_id int,
-  FOREIGN KEY (department)
-  REFERENCES department(id)
-  ON DELETE SET NULL
+  department_id int
+ -- FOREIGN KEY (department)
+ -- REFERENCES department(id)
+  -- ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name varchar(30),
   last_name varchar(30),
-  role_id INT
+  role_id INT,
   manager_id int
-  foreign key (role_id),
-  foreign key (manager_id)
+ -- FOREIGN KEY (role)
+ -- REFERENCES role(id)
 );
--- db/seed.sql
+++ db/seed.sql
@@ -1,20 +1,18 @@
-
-
-insert into department(name)
+insert into department (name)
 values ("Marketing"),
        ("Finance"),
        ("Human Resources"),
        ("Information Technology"),
        ("Operations");
 
-insert into role(title, salary, department_id)
-values ("Marketing assistant", 50000, 1)
-       ("Principle accountant", 35000, 2)
-       ("Human Resource Specialist", 65000, 3)
-       ("IT Manager", 85000, 4)
-       ("Operations manager", 55000 5)
+insert into roleE (title, salary, department_id)
+values ("Marketing assistant", 50000, 1),
+       ("Principle accountant", 35000, 2),
+       ("Human Resource Specialist", 65000, 3),
+       ("IT Manager", 85000, 4),
+       ("Operations manager", 55000, 5);
 
-insert into employee(first_name, last_name, role_id, manager_id)
-values ("Emily", "Burger", 1, 5)
-       ("Kendra", "Forster", 3, 1)
-       ("Issac", "Hulme", 2, 3)
+insert into employee (first_name, last_name, role_id, manager_id)
+values ("Emily", "Burger", 1, 5),
+       ("Kendra", "Forster", 3, 1),
+       ("Issac", "Hulme", 2, 3);
